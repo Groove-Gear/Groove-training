38c7e3599980999acd7758475e16e1e99e626f95
大住遥 <oosumi-h@groove-gear.jp>
2015-08-10 18:22:11 +0900

    完了

---
+++ oosumi/step2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+	<meta charset="UTF-8">
+</head>
+    <body>
+<?php
+$name = 大住遥;
+const BIRTHDAY = '1989年4月10日';
+$sélf_introdúction = '大住遥申とします。宜しくお願い致します。';
+$Hobby1 = 'ギター';
+$Hobby2 = '読書';
+
+echo '-自己紹介-<br/>';
+echo "【名前】{$name}<br/>";
+echo "【生年月日】".BIRTHDAY." <br/>";
+echo "【一言】{$sélf_introdúction}</br>";
+echo "【趣味】趣味は{$Hobby1}と{$Hobby2}です。";
+
+?>
+    </body>
+</html>
+
