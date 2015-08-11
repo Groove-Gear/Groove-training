58caa49387249dac9cdf7c28240dd96ae0b60a92
大住遥 <oosumi-h@groove-gear.jp>
2015-08-11 13:38:16 +0900

    修正

--- oosumi/step3/index.php
+++ oosumi/step3/index.php
@@ -5,18 +5,22 @@
 </head>
     <body>
 <?php
-$name = 大住遥;
+$name = '大住遥';
 const BIRTHDAY = '1989年4月10日';
 $sélf_introdúction = '初めまして。大住と申します。まだまだ何も出来ませんが頑張っていきたいと思います。';
-$Hobby1 = 'ギター';
-$Hobby2 = '読書';
+$hobby1 = 'ギター';
+$hobby2 = '読書';
+$today = Date("Y/m/d/h/i/s");
+
+$name1 = mb_substr($name, 0, 2);
+$name2 = mb_substr($name, 2, 1);
 
 echo '-自己紹介-<br/>';
-echo "【名前】{$name}<br/>";
+echo "【現在時刻】{$today}<br/>";
+echo "【名前】{$name1}{$name2}<br/>";
 echo "【生年月日】".BIRTHDAY." <br/>";
 echo "【一言】{$sélf_introdúction}</br>";
-echo "【趣味】趣味は{$Hobby1}と{$Hobby2}です。";
-
+echo "【趣味】趣味は{$hobby1}と{$hobby2}です。";
 ?>
     </body>
 </html>
