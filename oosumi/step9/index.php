844e022f7f1ea06ee0d9b40ef9abfc95d1eb2ff8
大住遥 <oosumi-h@groove-gear.jp>
2015-08-21 11:08:57 +0900

    html宣言追加

---
+++ oosumi/step9/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+        <meta charset="UTF-8">
+    </head>
+<?php
+
+
+
