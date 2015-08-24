758d90add9f7f32951fc3e4bfede771e1bf821b8
大住遥 <oosumi-h@groove-gear.jp>
2015-08-24 16:15:09 +0900

    終わりました。

--- oosumi/step9/index.php
+++ oosumi/step9/index.php
@@ -5,8 +5,8 @@
     </head>
     <?php
     $animal = array('猫', '犬', '兎', '鼠', '猿', '鳥', '魚', '馬', '牛', '鹿');
-    foreach ($animal as $value) {
-        echo "この動物は{$value}です。";
+    foreach ($animal as $val) {
+        echo "この動物は{$val}です。";
     }
     ?>
     <br>
@@ -14,42 +14,46 @@
     <?php
     $familyname = '山田';
     $sélf_introdúction = '宜しくお願いします';
+    $today = Date("Y/m/d/h/i/s");
 
-    $prfiles = array(
-        $prfile1 = array(
+    $profiles = array(
+         array(
     '紹介' => '-自己紹介-<br/>',
     '時刻' => "【現在時刻】{$today}<br/>",
     '姓' => "【姓】$familyname<br/>",
     '名' => "【名】一郎<br/>",
     '歳' => "【生年月日】1月1日 <br/>",
     '一言' => "【一言】{$sélf_introdúction}</br>",
-    '趣味' => "【趣味】将棋"),
-        $prfile2 = array(
+    '趣味' => "【趣味】将棋<br/><br/>"),
+         array(
     '紹介' => '-自己紹介-<br/>',
     '時刻' => "【現在時刻】{$today}<br/>",
     '姓' => "【姓】$familyname<br/>",
     '名' => "【名】次郎<br/>",
     '歳' => "【生年月日】2月1日 <br/>",
     '一言' => "【一言】{$sélf_introdúction}</br>",
-    '趣味' => "【趣味】チェス"),
-        $prfile3 = array(
+    '趣味' => "【趣味】チェス<br/><br/>"),
+         array(
     '紹介' => '-自己紹介-<br/>',
     '時刻' => "【現在時刻】{$today}<br/>",
     '姓' => "【姓】$familyname<br/>",
     '名' => "【名】三郎<br/>",
     '歳' => "【生年月日】3月1日 <br/>",
     '一言' => "【一言】{$sélf_introdúction}</br>",
-    '趣味' => "【趣味】花札"),
-        $prfile4 = array(
+    '趣味' => "【趣味】花札<br/><br/>"),
+         array(
     '紹介' => '-自己紹介-<br/>',
     '時刻' => "【現在時刻】{$today}<br/>",
     '姓' => "【姓】$familyname<br/>",
     '名' => "【名】史郎<br/>",
     '歳' => "【生年月日】4月1日<br/>",
     '一言' => "【一言】{$sélf_introdúction}</br>",
-    '趣味' => "【趣味】ダーツ")
+    '趣味' => "【趣味】ダーツ<br/><br/>")
     );
 
-    foreach ($prfiles as $key => $value){
-        echo "[$key]";
+    foreach ($profiles as $profile){
+       foreach ($profile as $key => $value) {
+            echo "{$key}:{$value}"; 
+        }
+        
     }
