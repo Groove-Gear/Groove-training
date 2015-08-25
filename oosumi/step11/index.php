bfa42fa04e632171d924b68bb64e602bfbd0c7ce
大住遥 <oosumi-h@groove-gear.jp>
2015-08-25 17:15:41 +0900

    出来ました。

--- oosumi/step11/index.php
+++ oosumi/step11/index.php
@@ -1,8 +1,34 @@
-<?php
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        
+        <form method="POST" action = "result.php">
+            名前：　　
+            <input type="text" name="name" size="15" />
+            <input type="submit" value="送信" />
+            <br>
+            生年月日：
+            <input type="text" name="birthday" size="15" />
+            <input type="submit" value="送信" />
+            <br>
+            一言：　　
+            <input type="text" name="self" size="15" />
+            <input type="submit" value="送信" />
+            <br>
+            趣味：　　
+            <input type="text" name="hobby" size="15" />
+            <input type="submit" value="送信" />
+            
+            
+            
+        </from>
+</body>
+</html>
+        
+        <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+        
