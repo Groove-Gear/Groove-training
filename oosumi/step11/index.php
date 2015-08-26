3b05e39cbe28da1f9fcdfe42524e640345cc5a8f
大住遥 <oosumi-h@groove-gear.jp>
2015-08-26 10:44:46 +0900

    送信ボタンを一つにしました。

--- oosumi/step11/index.php
+++ oosumi/step11/index.php
@@ -8,15 +8,15 @@
         <form method="POST" action = "result.php">
             名前：　　
             <input type="text" name="name" size="15" />
-            <input type="submit" value="送信" />
+            
             <br>
             生年月日：
             <input type="text" name="birthday" size="15" />
-            <input type="submit" value="送信" />
+            
             <br>
             一言：　　
             <input type="text" name="self" size="15" />
-            <input type="submit" value="送信" />
+            
             <br>
             趣味：　　
             <input type="text" name="hobby" size="15" />
